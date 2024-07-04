7e55a6f1234aaf7264223171cf27eb4f799151bd
Krystian <dsw52883@student.dsw.edu.pl>
2024-07-04 13:08:14 +0200

    Update LAB 3.sh

---
+++ LAB 3.sh
@@ -0,0 +1,26 @@
+Komendy na tutorial 
+
+1. 
+$git commit
+$git commit
+
+2.
+$git branch bugfix
+$git checkout bugfix
+
+3. 
+$Git branch bugfix
+$git checkout bugfix
+$git commit
+$git checkout main
+$git commit
+$git merge bugfix
+
+4.
+$git branch bugfix
+$git checkout bugfix
+$git commit
+$git checkout main
+$git commit
+$git checkout bugfix
+$git rebase main
